53abd8293c865b84c5ba0307f550a0ab4546202f
Doux Salem kayembe <76118979+douxsalemk@users.noreply.github.com>
2022-08-13 04:27:36 -0300

    feat: add postgress container

--- db/db.go
+++ db/db.go
@@ -2,14 +2,16 @@ package db
 
 import (
 	"database/sql"
+	"log"
 
 	_ "github.com/lib/pq"
 )
 
 func ConectaComBancoDeDados() *sql.DB {
-	conexao := "user=postgres dbname=alura_loja password=12345678 host=localhost sslmode=disable"
+	conexao := "user=postgres dbname=web password=postgres host=localhost sslmode=disable"
 	db, err := sql.Open("postgres", conexao)
 	if err != nil {
+		log.Println("FALHOU")
 		panic(err.Error())
 	}
 	return db
